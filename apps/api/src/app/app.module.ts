f1a877b8e1a618e97022d9d8a7c63316ae9ffc3d
Angel <mario.velez@sofka.com.co>
2021-04-14 21:20:13 -0500

    feat: implement NestJS API, Core library and environment configuration

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CoreModule } from "@petproject/core";
+
+@Module({
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ libs/core/src/lib/core.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from "@nestjs/config";
+import { validationSchema } from "./config/validation";
+import { configuration } from "./config/configuration";
+
+@Module({
+  imports : [ConfigModule.forRoot({
+    isGlobal : true,
+    load : [configuration],
+    validationSchema,
+  })],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class CoreModule {}
